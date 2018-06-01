73d6a38e1c0823a5c4684a69a2bd65f1a40e9525
Garrett Moore <thegarrettmoore@gmail.com>
2018-05-31 20:02:07 -0500

    adding port check

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"os"
+
 	"github.com/labstack/echo"
 
 	"github.com/garrettmm/garrettmoore/pkg/middleware"
@@ -13,5 +15,12 @@ func Init() {
 	routes.Register(Server)
 	middleware.Register(Server)
 
-	Server.Logger.Fatal(Server.Start(":1323"))
+	Server.Logger.Fatal(Server.Start(getEnv("PORT", ":1323")))
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return ":" + value
+	}
+	return fallback
 }
